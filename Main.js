0858cb108a6414fcfb8c85dc8cf408ff1040631f
Daniyal <166503304+Rikatsu17@users.noreply.github.com>
2025-06-05 23:33:45 +0500

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,17 @@
+const animeList = [
+  {
+    title: "Attack on Titan",
+    genres: ["Action", "Drama"],
+    poster: "https://upload.wikimedia.org/wikipedia/en/2/20/Attack_on_Titan_cover.jpg"
+  },
+  {
+    title: "Death Note",
+    genres: ["Thriller", "Mystery"],
+    poster: "https://upload.wikimedia.org/wikipedia/en/6/6f/Death_Note_Vol_1.jpg"
+  },
+  {
+    title: "Naruto",
+    genres: ["Adventure", "Fantasy"],
+    poster: "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg"
+  }
+];
